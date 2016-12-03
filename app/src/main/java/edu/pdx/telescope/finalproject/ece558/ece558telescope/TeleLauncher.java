f835ea33f0f8238ffb8f3ee2549b5bec650aa636
NishadSaraf <nns4@pdx.edu>
2016-12-02 18:25:55 -0800

    Added random string

--- app/src/main/java/edu/pdx/telescope/finalproject/ece558/ece558telescope/TeleLauncher.java
+++ app/src/main/java/edu/pdx/telescope/finalproject/ece558/ece558telescope/TeleLauncher.java
@@ -3,12 +3,12 @@ package edu.pdx.telescope.finalproject.ece558.ece558telescope;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TeleLauncher extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String random = "Hello from the other side!!";
+        String random = "Hello World!!";
     }
 }
