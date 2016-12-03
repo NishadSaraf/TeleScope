d9bbd3d994b2eddf97c7684798a3f794e5ac26e7
jaisilmj <jaisil.mj@gmail.com>
2016-12-02 18:02:32 -0800

    Initial commit

---
+++ app/src/main/java/edu/pdx/telescope/finalproject/ece558/ece558telescope/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.pdx.telescope.finalproject.ece558.ece558telescope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
