a126b8f95d4ef72e14e9cb98ca641f2aaa5a86ac
jaisilmj <jaisil.mj@gmail.com>
2016-12-04 18:35:03 -0800

    Added bluetooth refereneces Instantiated blueetooth references Added onResume override

--- app/src/main/java/edu/pdx/telescope/finalproject/ece558/ece558telescope/TeleLauncher.java
+++ app/src/main/java/edu/pdx/telescope/finalproject/ece558/ece558telescope/TeleLauncher.java
@@ -1,16 +1,43 @@
 package edu.pdx.telescope.finalproject.ece558.ece558telescope;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 
 public class TeleLauncher extends AppCompatActivity {
 
     public static final String TAG= TeleLauncher.class.getSimpleName();
 
+    //References for bluetooth management
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private SparseArray<BluetoothDevice> mDevices;  //stores all the available devices
+    private BluetoothGatt mConnectedGatt;           //represents the connected device
+
+    private Handler mHandler = new Handler();       //Handles updates to UI
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String random = "Hello World!!";
+
+        //Instantiating bluetooth references
+        mBluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+        mDevices = new SparseArray<BluetoothDevice>();
+    }
+
+
+    /**
+     * Actions to be done while resuming activity
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
